6a3cc1c465c44630b26a3a00b1a791f7162afe24
Anthony Nahas <anthony.na@hotmail.de>
2020-01-15 21:08:30 +0100

    fix(project): added modules imports in the demo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Angulartics2Module} from 'angulartics2';
+import {MarkdownModule} from 'ngx-markdown';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {HttpClientModule} from '@angular/common/http';
+import {MatFabMenuModule} from '@angular-material-extensions/fab-menu';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    BrowserAnimationsModule
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    Angulartics2Module.forRoot(),
+    MarkdownModule.forRoot(),
+    MatFabMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
