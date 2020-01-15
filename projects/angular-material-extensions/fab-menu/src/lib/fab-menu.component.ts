9730f6c8ca1026ae22737cb25970638806a7eb9b
Anthony Nahas <anthony.na@hotmail.de>
2020-01-15 12:16:02 +0100

    feat(lib): generated the library

---
+++ projects/angular-material-extensions/fab-menu/src/lib/fab-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-fab-menu',
+  template: `
+    <p>
+      fab-menu works!
+    </p>
+  `,
+  styles: []
+})
+export class FabMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/angular-material-extensions/fab-menu/src/lib/fab-menu.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FabMenuComponent } from './fab-menu.component';
+
+
+
+@NgModule({
+  declarations: [FabMenuComponent],
+  imports: [
+  ],
+  exports: [FabMenuComponent]
+})
+export class FabMenuModule { }
