88b061cba72dc360cc3881c49952ffc9e98f41d5
Anthony Nahas <anthony.na@hotmail.de>
2020-01-15 12:16:52 +0100

    feat(project): added angular material to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' })
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
