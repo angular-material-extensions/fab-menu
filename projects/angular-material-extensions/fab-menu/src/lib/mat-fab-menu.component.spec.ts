062f7f506fc60c1069716cde18c00dbda9d87dec
Anthony Nahas <anthony.na@hotmail.de>
2020-11-20 00:23:23 +0100

    fix(lib): upgraded angular to v11

--- projects/angular-material-extensions/fab-menu/src/lib/mat-fab-menu.component.spec.ts
+++ projects/angular-material-extensions/fab-menu/src/lib/mat-fab-menu.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {MatFabMenuComponent} from './mat-fab-menu.component';
 
@@ -6,7 +6,7 @@ describe('FabMenuComponent', () => {
   let component: MatFabMenuComponent;
   let fixture: ComponentFixture<MatFabMenuComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [MatFabMenuComponent]
     })
